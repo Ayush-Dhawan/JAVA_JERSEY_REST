b5b0857275c5f64fc759aef96318276f37523c9c
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-10-02 14:36:23 +0530

    maintaining single session for all queries per user

--- src/main/java/org/Repositories/AlienRepository.java
+++ src/main/java/org/Repositories/AlienRepository.java
@@ -13,18 +13,13 @@ import java.util.List;
 
 public class AlienRepository {
     private static List<Alien> alien;
+    private Session session;
 
     public AlienRepository() {
-//        Alien alien1 = new Alien(1, "alien1", 101);
-//        Alien alien2 = new Alien(2, "alien2", 102);
-//        Alien alien3 = new Alien(3, "alien3", 103);
-//        Alien alien4 = new Alien(4, "alien4", 104);
-//        Alien alien5 = new Alien(5, "alien5", 105);
-//        alien = new ArrayList<>(Arrays.asList(alien1, alien2, alien3, alien4, alien5));
+        session = HibernateUtil.getSession();
     }
 
     public Alien getAlienById(int id) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query q = session.createQuery("from Alien where id = :id");
             q.setParameter("id", id);
             Alien alien = (Alien) q.uniqueResult();
@@ -32,29 +27,21 @@ public class AlienRepository {
                 System.out.println("No Alien found with id: " + id);  // Optional: Log or handle the absence of the Alien
             }
             return alien;
-        }
     }
 
     public List<Alien> getAllAliens() {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
             Query q = session.createQuery("from Alien");
             List<Alien> aliens = q.list();
-            session.close();
             return aliens;
-        }
-
     }
 
     public List<Alien> addNewAlien(Alien a) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
             Transaction tx = session.beginTransaction();
             Query q = session.createQuery("from Alien");
             List<Alien> aliens = q.list();
             session.save(a);
             tx.commit();
-            session.close();
             return alien;
-        }
     }
 
     public void updateAlienInfo(String oldName, String newName) {
--- src/main/java/org/apicode/HibernateUtil.java
+++ src/main/java/org/apicode/HibernateUtil.java
@@ -1,20 +1,21 @@
 package org.apicode;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
+    private static final Session session;
 
     static {
         try {
-            sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class).buildSessionFactory();
+            session = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class).buildSessionFactory().openSession();
         } catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
         }
     }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+    public static Session getSession() {
+        return session;
     }
 }
